460b1450751ff4e2f4bda8822d2080fb630758fd
arkadem1 <zander@bontage.co.za>
2019-07-28 14:11:19 +0200

    This is the beta version of the profile editor screen.

---
+++ WindowsFormsApp1/frm_Edit_Profile.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frm_Edit_Profile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Edit_Profile));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbx_Full_Name = new System.Windows.Forms.TextBox();
+            this.txtbx_Username = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbx_Email = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mtxtbx_Mobile_Number = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtbx_Confirm_Password = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtbx_Password = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.chkbx_Change = new System.Windows.Forms.CheckBox();
+            this.btn_Change_Profile_Picture = new System.Windows.Forms.Button();
+            this.picbx_User = new System.Windows.Forms.PictureBox();
+            this.btn_Accept_Changes = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picbx_User)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1241, 75);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(3, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Edit Your Profile";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Accept_Changes);
+            this.groupBox1.Controls.Add(this.picbx_User);
+            this.groupBox1.Controls.Add(this.btn_Change_Profile_Picture);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.mtxtbx_Mobile_Number);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtbx_Email);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtbx_Username);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtbx_Full_Name);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(801, 226);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Full Name";
+            // 
+            // txtbx_Full_Name
+            // 
+            this.txtbx_Full_Name.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_Full_Name.Location = new System.Drawing.Point(11, 44);
+            this.txtbx_Full_Name.Name = "txtbx_Full_Name";
+            this.txtbx_Full_Name.Size = new System.Drawing.Size(210, 22);
+            this.txtbx_Full_Name.TabIndex = 1;
+            // 
+            // txtbx_Username
+            // 
+            this.txtbx_Username.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_Username.Location = new System.Drawing.Point(11, 93);
+            this.txtbx_Username.Name = "txtbx_Username";
+            this.txtbx_Username.Size = new System.Drawing.Size(210, 22);
+            this.txtbx_Username.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            // 
+            // txtbx_Email
+            // 
+            this.txtbx_Email.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_Email.Location = new System.Drawing.Point(11, 142);
+            this.txtbx_Email.Name = "txtbx_Email";
+            this.txtbx_Email.Size = new System.Drawing.Size(210, 22);
+            this.txtbx_Email.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(7, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(7, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Mobile Number";
+            // 
+            // mtxtbx_Mobile_Number
+            // 
+            this.mtxtbx_Mobile_Number.Font = new System.Drawing.Font("Arial Narrow", 9.75F);
+            this.mtxtbx_Mobile_Number.Location = new System.Drawing.Point(11, 191);
+            this.mtxtbx_Mobile_Number.Mask = "(999) 000-0000";
+            this.mtxtbx_Mobile_Number.Name = "mtxtbx_Mobile_Number";
+            this.mtxtbx_Mobile_Number.Size = new System.Drawing.Size(210, 22);
+            this.mtxtbx_Mobile_Number.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkbx_Change);
+            this.groupBox2.Controls.Add(this.txtbx_Confirm_Password);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.txtbx_Password);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Location = new System.Drawing.Point(553, 44);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 131);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtbx_Confirm_Password
+            // 
+            this.txtbx_Confirm_Password.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_Confirm_Password.Location = new System.Drawing.Point(10, 89);
+            this.txtbx_Confirm_Password.Name = "txtbx_Confirm_Password";
+            this.txtbx_Confirm_Password.Size = new System.Drawing.Size(210, 22);
+            this.txtbx_Confirm_Password.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 65);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Confirm Password";
+            // 
+            // txtbx_Password
+            // 
+            this.txtbx_Password.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbx_Password.Location = new System.Drawing.Point(10, 40);
+            this.txtbx_Password.Name = "txtbx_Password";
+            this.txtbx_Password.Size = new System.Drawing.Size(210, 22);
+            this.txtbx_Password.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(6, 16);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 20);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Password";
+            // 
+            // chkbx_Change
+            // 
+            this.chkbx_Change.AutoSize = true;
+            this.chkbx_Change.Location = new System.Drawing.Point(154, 17);
+            this.chkbx_Change.Name = "chkbx_Change";
+            this.chkbx_Change.Size = new System.Drawing.Size(66, 17);
+            this.chkbx_Change.TabIndex = 8;
+            this.chkbx_Change.Text = "Change!";
+            this.chkbx_Change.UseVisualStyleBackColor = true;
+            // 
+            // btn_Change_Profile_Picture
+            // 
+            this.btn_Change_Profile_Picture.Location = new System.Drawing.Point(317, 188);
+            this.btn_Change_Profile_Picture.Name = "btn_Change_Profile_Picture";
+            this.btn_Change_Profile_Picture.Size = new System.Drawing.Size(153, 25);
+            this.btn_Change_Profile_Picture.TabIndex = 9;
+            this.btn_Change_Profile_Picture.Text = "Change Profile Picture";
+            this.btn_Change_Profile_Picture.UseVisualStyleBackColor = true;
+            // 
+            // picbx_User
+            // 
+            this.picbx_User.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picbx_User.Location = new System.Drawing.Point(330, 44);
+            this.picbx_User.Name = "picbx_User";
+            this.picbx_User.Size = new System.Drawing.Size(128, 128);
+            this.picbx_User.TabIndex = 10;
+            this.picbx_User.TabStop = false;
+            // 
+            // btn_Accept_Changes
+            // 
+            this.btn_Accept_Changes.Location = new System.Drawing.Point(597, 181);
+            this.btn_Accept_Changes.Name = "btn_Accept_Changes";
+            this.btn_Accept_Changes.Size = new System.Drawing.Size(157, 38);
+            this.btn_Accept_Changes.TabIndex = 11;
+            this.btn_Accept_Changes.Text = "Apply Changes";
+            this.btn_Accept_Changes.UseVisualStyleBackColor = true;
+            // 
+            // frm_Edit_Profile
+            // 
+            this.AcceptButton = this.btn_Accept_Changes;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 312);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Edit_Profile";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edit Profile";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picbx_User)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Change_Profile_Picture;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkbx_Change;
+        private System.Windows.Forms.TextBox txtbx_Confirm_Password;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtbx_Password;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox mtxtbx_Mobile_Number;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbx_Email;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbx_Username;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbx_Full_Name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Accept_Changes;
+        private System.Windows.Forms.PictureBox picbx_User;
+    }
+}
---
+++ WindowsFormsApp1/frm_Edit_Profile.cs
@@ -0,0 +1,22 @@
+﻿//Hello World, my name is Zander Boonzaaier...What now?
+//Perspective, my dude.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frm_Edit_Profile : Form
+    {
+        public frm_Edit_Profile()
+        {
+            InitializeComponent();
+        }
+    }
+}
